e76437deef968aaf927d3f15f70ab4b9cfb8ff5c
Ali Haider Swccho <swccho263@gmail.com>
2025-01-14 12:32:21 +0600

    screens

---
+++ Screens/Authentication/HomeScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function HomeScreen(){
+    return (
+        <View>
+            <Text>Authentication Home Screen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen;
