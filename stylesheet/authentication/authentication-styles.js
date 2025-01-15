78f8527d541213e3ce046d02d1fd5e4f64e4bf69
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2025-01-15 13:22:56 +0600

    [002- login] - login ui creating

---
+++ stylesheet/authentication/authentication-styles.js
@@ -0,0 +1,33 @@
+import {StyleSheet} from "react-native";
+import {AuthenticationInterface} from "../stylesInterfaces/auth-interface";
+
+export const authenticationStyles: AuthenticationInterface = StyleSheet.create({
+    authContainer: {
+        padding:20,
+        display:"flex",
+        alignItems:"center"
+    },
+    logo:{
+        width:200,
+        height:120,
+        marginBottom:30,
+        marginTop:40,
+        alignItems:'center',
+        position:"relative",
+    },
+    authInput: {
+        width: '90%',
+        backgroundColor: '#fff',
+        borderRadius: 15,
+        padding: 20,
+        fontSize: 16,
+        color: '#333',
+        marginBottom: 20,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.2,
+        shadowRadius: 1,
+        elevation: 3,
+    },
+  
+})
