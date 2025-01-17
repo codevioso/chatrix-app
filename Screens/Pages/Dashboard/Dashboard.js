b5168d5e07456b50837633447bac44b59719d945
Ali Haider Swccho <swccho263@gmail.com>
2025-01-17 18:29:27 +0600

    [003]-code update

---
+++ Screens/Pages/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import {Text, View} from "react-native";
+
+const DashboardScreen = ({navigation}) => {
+    return (
+        <>
+           <View>
+               <Text>Welcome</Text>
+               <Text>You have successfully logged in.</Text>
+           </View>
+        </>
+    )
+}
+
+export default DashboardScreen;
