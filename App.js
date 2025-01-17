52c54922576a6070486ada3fe5ebd69ad55edf13
Ali Haider Swccho <swccho263@gmail.com>
2025-01-17 14:55:40 +0600

    [003]-code updated

--- App.js
+++ App.js
@@ -7,34 +7,37 @@ import RegisterScreen from "./Screens/Authentication/Register";
 import ForgotScreen from "./Screens/Authentication/Forgot";
 import ResetScreen from "./Screens/Authentication/Reset";
 import ActivationScreen from "./Screens/Authentication/Activation";
+import AuthContextProvider from "./store/auth-context";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
         <>
-            <NavigationContainer>
-                <Stack.Navigator id={"Auth"} initialRouteName={'AuthHomeScreen'}>
-                    <Stack.Screen name={'AuthHomeScreen'} component={HomeScreen} options={{
-                        headerShown: false,
-                    }}/>
-                    <Stack.Screen name={'LoginScreen'} component={LoginScreen} options={{
-                        headerShown: false,
-                    }}/>
-                    <Stack.Screen name={'RegisterScreen'} component={RegisterScreen} options={{
-                        headerShown: false,
-                    }}/>
-                    <Stack.Screen name={'ForgotScreen'} component={ForgotScreen} options={{
-                        headerShown: false,
-                    }}/>
-                    <Stack.Screen name={'ResetScreen'} component={ResetScreen} options={{
-                        headerShown: false,
-                    }}/>
-                    <Stack.Screen name={'ActivationScreen'}  component={ActivationScreen} options={{
-                        headerShown: false,
-                    }}/>
-                </Stack.Navigator>
-            </NavigationContainer>
+            <AuthContextProvider>
+                <NavigationContainer>
+                    <Stack.Navigator id={"Auth"} initialRouteName={'AuthHomeScreen'}>
+                        <Stack.Screen name={'AuthHomeScreen'} component={HomeScreen} options={{
+                            headerShown: false,
+                        }}/>
+                        <Stack.Screen name={'LoginScreen'} component={LoginScreen} options={{
+                            headerShown: false,
+                        }}/>
+                        <Stack.Screen name={'RegisterScreen'} component={RegisterScreen} options={{
+                            headerShown: false,
+                        }}/>
+                        <Stack.Screen name={'ForgotScreen'} component={ForgotScreen} options={{
+                            headerShown: false,
+                        }}/>
+                        <Stack.Screen name={'ResetScreen'} component={ResetScreen} options={{
+                            headerShown: false,
+                        }}/>
+                        <Stack.Screen name={'ActivationScreen'}  component={ActivationScreen} options={{
+                            headerShown: false,
+                        }}/>
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </AuthContextProvider>
         </>
     );
 }
